ce3b0828408904d356a88bce20b57d289b49a9ed
knarfbouman <knarfbouman@live.nl>
2015-06-07 11:40:26 +0200

    Authorizemanager done
    nu nog een activity maken, waarin een webview wordt geinstantieerd, en
    een object van authorizationmanager.

--- Source/TwitterClient/src/nl/drijfhout/twitterclient/login/AuthorizationManager.java
+++ Source/TwitterClient/src/nl/drijfhout/twitterclient/login/AuthorizationManager.java
@@ -1,5 +1,8 @@
 package nl.drijfhout.twitterclient.login;
 
+import java.util.concurrent.ExecutionException;
+
+import nl.drijfhout.twitterclient.tasks.GetRequestTokenTask;
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.OAuthProvider;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
@@ -9,21 +12,37 @@ public class AuthorizationManager {
 
 	private OAuthConsumer consumer;
 	private OAuthProvider provider;
-	private String url;
 	private	static final String	CONSUMER_KEY	= "k4xSeNaViDzSHgN1pV4822y8Z";
 	private	static	final	String	CONSUMER_SECRET	= "hcxPzrRWMKmCFLmKLWaegEaMfKPp8jyFCE3SnQQfojrRxvBGne";
 	private	static	final	String	OAUTH_REQUEST_URL	= "https://api.twitter.com/oauth/request_token";
 	private	static	final	String	OAUTH_ACCESSTOKEN_URL	= "https://api.twitter.com/oauth/access_token";
-	private	static	final	String	OAUTH_AUTHORIZE_URL	= "https://api.twitter.com/oauth/authorize";
-	private	static final String	OAUTH_CALLBACK_URL	= "http://www.saxion.nl/pgrtwitter/authenticated";
+	private	 String	OAUTH_AUTHORIZE_URL	= "https://api.twitter.com/oauth/authorize?oauth_token=";
+	private String requestToken;
+	
 	//public boolean isUserLoggedIn(){
 	//	
 	//}
+	public String getRequestToken(){
+		return requestToken;
+	}
+	
 	
 	public void init(){
 		consumer=	new CommonsHttpOAuthConsumer(CONSUMER_KEY,	CONSUMER_SECRET);	
 		provider	=	new CommonsHttpOAuthProvider(	OAUTH_REQUEST_URL, OAUTH_ACCESSTOKEN_URL,	OAUTH_AUTHORIZE_URL);
+		GetRequestTokenTask getrequest = new GetRequestTokenTask(consumer);
 		
+		
+		try {
+			requestToken = getrequest.execute(provider).get();
+			OAUTH_AUTHORIZE_URL += requestToken;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
---
+++ Source/TwitterClient/src/nl/drijfhout/twitterclient/tasks/GetRequestTokenTask.java
@@ -0,0 +1,42 @@
+package nl.drijfhout.twitterclient.tasks;
+
+
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.exception.OAuthNotAuthorizedException;
+import android.os.AsyncTask;
+
+public class GetRequestTokenTask extends AsyncTask<OAuthProvider,Void,String> {
+
+	private	static final String	OAUTH_CALLBACK_URL	= "http://www.saxion.nl/pgrtwitter/authenticated";
+	private OAuthConsumer consumer;
+	public GetRequestTokenTask(OAuthConsumer consumer){
+		this.consumer = consumer;
+	}
+	
+	@Override
+	protected String doInBackground(OAuthProvider... params) {
+		String url = "";
+		try {
+			 url = params[0].retrieveRequestToken(consumer, OAUTH_CALLBACK_URL);
+		} catch (OAuthMessageSignerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthNotAuthorizedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthExpectationFailedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthCommunicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return url;
+	}
+
+}
